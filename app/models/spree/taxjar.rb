1b7eb5fadcbb1ea2c9f098ab3816c68b24b11307
Denis <visible@jumph4x.net>
2019-11-12 15:49:27 -0800

    Not all addresses have state objects, some just names

--- app/models/spree/taxjar.rb
+++ app/models/spree/taxjar.rb
@@ -90,7 +90,7 @@ module Spree
       end
 
       def tax_address_state_abbr
-        tax_address.state.abbr
+        (tax_address.state && tax_address.state.abbr)  || tax_address.state_name
       end
 
       def tax_address_city
